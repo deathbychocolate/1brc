2b1ca2ba159dafc0a1ab3ba400d1851f022935ce
Damianos Vonapartis <damianosvon@gmail.com>
2025-01-18 16:50:45 +0100

    feat: Add minimum, mean, and maximum functions.

--- 1brc/src/main.py
+++ 1brc/src/main.py
@@ -1,6 +1,21 @@
 """The only file we will ever need."""
 
 
+def minimum() -> float:
+    minimum: float = 0.0
+    return minimum
+
+
+def mean() -> float:
+    mean: float = 0.0
+    return mean
+
+
+def maximum() -> float:
+    maximum: float = 0.0
+    return maximum
+
+
 def main() -> None:
     print("Hello, world!")
 
