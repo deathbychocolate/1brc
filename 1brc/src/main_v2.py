0636d0e6d594e37060d8315212b75605c2aeea1a
Damianos Vonapartis <damianosvon@gmail.com>
2025-01-25 22:59:28 +0100

    refactor: Speed up the algorithm by processing bytes rather than decoded characters, use list comprehension, and avoid as many string operations as possible.

--- 1brc/src/main_v2.py
+++ 1brc/src/main_v2.py
@@ -4,47 +4,33 @@ from os import path
 from timeit import timeit
 
 
-def parse_weather_station_data() -> None:  # this will likely take ~20 mins for 1 billion rows
-    """This function holds all the logic we need."""
-    filepath: str = str(path.dirname(path.abspath(__file__))) + "/data/weather_stations.csv"
-    with open(file=filepath, mode="r", encoding="utf8") as filepointer:
-        rows: list[str] = filepointer.read().splitlines()
-        rows = sorted(rows)  # sort with O(nlog(n)) algorithm
+def parse_weather_station_data() -> None:
+    """This function holds all the logic we need.
 
-        # Construct a dictionary 'weather_station_data' so that we group all temps together by city.
-        # The city name is the key, the value is a list of floats.
-        # Do all conversions needed.
-        weather_station_data: dict[str, list[float]] = {}
-        for row in rows:
-            left: str = ""
-            right: str = ""
-            left, right = row.split(";", maxsplit=1)
-            city_name: str = left
-            city_temperature: float = float(right)
+    This function constructs a dictionary called 'weather_station_data'.
+    The dictionary groups all temps together by city name where each city has
+    all temperatures accumulated.
+    """
+
+    filepath: str = (str(path.dirname(path.abspath(__file__))) + "/data/weather_stations.csv")
+    weather_station_data: dict[bytes, list[float]] = {}
+    with open(file=filepath, mode="rb") as filepointer:
+        for row in filepointer:
+            city_name, city_temperature = row.split(b";")
+            city_temperature = city_temperature.rstrip()
             if weather_station_data.get(city_name) is None:
-                weather_station_data[city_name] = []
-                weather_station_data[city_name].append(city_temperature)
+                weather_station_data[city_name] = [float(city_temperature)]
             else:
-                weather_station_data[city_name].append(city_temperature)
+                weather_station_data[city_name].append(float(city_temperature))
 
-        # now that the data is organized, process it to get min, mean, and max values
-        weather_station_data_processed: str = "{"
-        temperatures: list[float]
-        mean: float = -999.9
-        minimum: float = -999.9
-        maximum: float = -999.9
-        for city_name, temperatures in weather_station_data.items():
-            mean = sum(temperatures) / len(temperatures)
-            minimum = min(temperatures)
-            maximum = max(temperatures)
-            weather_station_data_processed += (
-                city_name + "=" + "/".join([str(minimum), str(mean), str(maximum)]) + ", "
-            )
+    # now that the data is organized, process it to get min, mean, and max values
+    weather_station_data_per_city = [
+        f"{city_name.decode()}={min(temperatures)}/{sum(temperatures) / len(temperatures)}/{max(temperatures)}"
+        for city_name, temperatures in weather_station_data.items()
+    ]
+    output: str = ", ".join(weather_station_data_per_city)
 
-        weather_station_data_processed = weather_station_data_processed.rstrip(", ")
-        weather_station_data_processed += "}"
-
-        # print(weather_station_data_processed)
+    # print("{",output,"}")
 
 
 def main() -> None:
