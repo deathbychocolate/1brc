695d64747b317b0ef5c7795c219ca0e1b5c20550
Damianos Vonapartis <damianosvon@gmail.com>
2025-01-26 21:27:13 +0100

    feat: Add version 3 of main.py.
    Significant gains made here with the usage of generator comprehension instead of list comprehension. We also cleverly made join() the consumer of that generator. Issues with memory arise from sorted() however. Sorting will likely need to be tackled when we are gathering the lines of data from the file (in the code directly above). Lastly it seems writing the code in a way that requires less memory read and writes, and freeing up more memory for the program are key parts to optimizing this. Another thing to try is parallel computation with concurrent futures and Process are avenues to explore.

---
+++ 1brc/src/main_v3.py
@@ -0,0 +1,46 @@
+"""The only file we will ever need."""
+
+from os import environ
+from timeit import timeit
+
+
+def parse_weather_station_data() -> None:
+    """This function holds all the logic we need.
+
+    This function constructs a dictionary called 'weather_station_data'.
+    The dictionary groups all temps together by city name where each city has
+    all temperatures accumulated.
+    """
+
+    filepath: str = environ["FILEPATH"]
+    weather_station_data: dict[bytes, list[float]] = {}
+    with open(file=filepath, mode="rb") as filepointer:
+        for row in filepointer:
+            city_name, city_temperature = row.rstrip().split(b";")
+            if not weather_station_data.get(city_name):
+                weather_station_data[city_name] = [float(city_temperature)]
+            else:
+                weather_station_data[city_name].append(float(city_temperature))
+
+    # now that the data is organized, process it to get min, mean, and max values
+    weather_station_data_per_city: str = ', '.join((
+        f"{city_name.decode()}={min(temperatures)}/{sum(temperatures) / len(temperatures)}/{max(temperatures)}"
+        for city_name, temperatures in sorted(weather_station_data.items())
+    ))
+    print("{", end="")
+    print(weather_station_data_per_city, end="")
+    print("}")
+
+def main() -> None:
+    """Call this function to run the program."""
+    # parse_weather_station_data()
+    wall_time_in_seconds: float = timeit(
+        stmt="parse_weather_station_data()",
+        setup="from main_v2 import parse_weather_station_data",
+        number=1,  # times we call the function
+    )
+    print(f"Wall time in seconds: {wall_time_in_seconds}")
+
+
+if __name__ == "__main__":
+    main()
