fd36bc069bb5acd675e97d2e60790ae6495c46cc
Damianos Vonapartis <damianosvon@gmail.com>
2025-02-07 21:07:24 +0100

    refactor: Improve performance, refactor, and clean up v4.

--- 1brc/src/main_v4.py
+++ 1brc/src/main_v4.py
@@ -5,7 +5,7 @@ from timeit import timeit
 from bisect import bisect_left, bisect_right
 
 
-def find_in_sorted_list(element: bytes, sorted_list: list) -> int:
+def find_element_in_sorted_list(sorted_list: list, element: bytes) -> int:
     """Locate the leftmost value exactly equal to `element`."""
     # https://docs.python.org/3/library/bisect.html
     i = bisect_left(sorted_list, element)
@@ -17,36 +17,29 @@ def find_in_sorted_list(element: bytes, sorted_list: list) -> int:
 def parse_weather_station_data() -> None:
     """This function holds all the logic we need.
 
-    This function constructs a dictionary called 'weather_station_data'.
-    The dictionary groups all temps together by city name where each city has
-    all temperatures accumulated.
+    We hoped to make some performance gains by removing the need of `sorted()`.
+    But we ended up creating a slower algorithm than `V3` likely due to using list `inserts`.
+
+    In detail, we construct 2 variables; `weather_station_names` and `weather_station_temps`.
+    They are lists that accept inserts of weather station data.
+    This data is inserted in sorted order by finding the correct point of insert via
+    the `bisect` module.
     """
 
     filepath: str = environ["FILEPATH"]
     weather_station_names: list[bytes] = []
     weather_station_temps: list[list[float]] = []
 
-    # initiate the lists, this is needed to use bisect correctly later.
-    # with open(file=filepath, mode="rb") as fp:
-    #     row = next(fp)
-    #     city_name, city_temperature = row.rstrip().split(b";")
-    #     weather_station_names.append(city_name)
-    #     weather_station_temps.append([float(city_temperature)])
-
     with open(file=filepath, mode="rb") as fp:
-        next(fp)  # skip first line
         index: int = -1
         for row in fp:
             city_name, city_temperature = row.rstrip().split(b";")
-            index = find_in_sorted_list(
-                element=city_name,
-                sorted_list=weather_station_names,
-            )  # is the city already in the list?
-            if index == -1:  # if city not found in list, add the city name and the temp
+            index = find_element_in_sorted_list(sorted_list=weather_station_names, element=city_name)
+            if index == -1:  # city not found
                 insertion_index: int = bisect_right(weather_station_names, city_name)
                 weather_station_names.insert(insertion_index, city_name)
                 weather_station_temps.insert(insertion_index, [float(city_temperature)])
-            else:  # add the temp only to the index found
+            else:  # city found
                 weather_station_temps[index].append(float(city_temperature))
 
     # now that the data is organized, process it to get min, mean, and max values
@@ -66,7 +59,7 @@ def main() -> None:
     # parse_weather_station_data()
     wall_time_in_seconds: float = timeit(
         stmt="parse_weather_station_data()",
-        setup="from main_v3 import parse_weather_station_data",
+        setup="from main_v4 import parse_weather_station_data",
         number=1,  # times we call the function
     )
     print(f"Wall time in seconds: {wall_time_in_seconds}")
